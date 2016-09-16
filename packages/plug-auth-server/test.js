ad4defcef2b079cd88f138800983427bb0c7a9ed
René Kooi <renee@kooi.me>
2016-09-16 11:33:10 +0200

    server: add protected route to example using express-jwt

--- packages/plug-auth-server/test.js
+++ packages/plug-auth-server/test.js
@@ -1,22 +1,32 @@
 const pas = require('./')
 const cors = require('cors')
+const expressJwt = require('express-jwt')
 const bodyParser = require('body-parser').json
 const app = require('express')()
+
+const secret = new Buffer('not secret')
 
 const engine = pas.authenticator({
   auth: {
     email: process.env.PLUGDJ_EMAIL,
     password: process.env.PLUGDJ_PASSWORD
   },
-  secret: 'not secret'
+  secret: secret
 })
 
 app.use(bodyParser())
+app.options('*', cors())
 
-app.options('/auth/login', cors())
 app.post('/auth/login', cors(), pas.express(engine))
-app.options('/', cors())
-app.get('/', cors(), (req, res) => res.json({ secret: 'hoi!' }))
+
+app.get('/',
+  cors(),
+  expressJwt({
+    secret: secret,
+    requestProperty: 'plugUser'
+  }),
+  (req, res) => res.json({ you: req.plugUser })
+)
 
 app.use((err, req, res, next) => {
   throw err
