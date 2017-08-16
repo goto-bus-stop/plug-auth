e01bbb25ea68010a371cf891f01a4ecad92c3ad3
Renée Kooi <renee@kooi.me>
2017-08-16 10:59:02 +0200

    Improve version sync script

--- version.js
+++ version.js
@@ -1,12 +1,14 @@
 const fs = require('fs')
+const spawn = require('child_process').spawnSync
 const join = require('path').join
-const version = require('./package.json').version
+const version = process.env.npm_package_version
 
 function bump(name, version) {
   const path = join(__dirname, 'packages', name, 'package.json')
   const pkg = JSON.parse(fs.readFileSync(path, 'utf8'))
   pkg.version = version
   fs.writeFileSync(path, JSON.stringify(pkg, null, 2) + '\n', 'utf8')
+  spawn('git', ['add', path])
 }
 
 bump('plug-auth-client', version)
